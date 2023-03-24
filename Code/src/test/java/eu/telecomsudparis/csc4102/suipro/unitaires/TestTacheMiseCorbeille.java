e4fc7788907bc31be96dd0f32e78f8cde0bd9246
Juliette DEBONO <juliette.debono@telecom-sudparis.eu>
2023-03-24 10:50:37 +0100

    modif

--- Code/src/test/java/eu/telecomsudparis/csc4102/suipro/unitaires/TestTacheMiseCorbeille.java
+++ Code/src/test/java/eu/telecomsudparis/csc4102/suipro/unitaires/TestTacheMiseCorbeille.java
@@ -8,7 +8,6 @@ import eu.telecomsudparis.csc4102.suipro.PeriodeDeTravail;
 import java.time.Duration;
 import java.time.Instant;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
